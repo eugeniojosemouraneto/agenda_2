78dd02deb3428171b4256a29b2e78679a520b906
Eugenio Jose Moura Neto <eugeniojosemouraneto092@gmail.com>
2024-04-28 22:38:34 -0300

    Paginação do projeto

--- agenda/views/home.py
+++ agenda/views/home.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from datetime import datetime
 from agenda.models import *
@@ -6,16 +7,21 @@ import locale
 
 locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
-
 def home(request):
     dia_hoje = datetime.now().strftime("%A, %d/%m/%Y")
 
     tarefas = Tarefa.atividades_do_mes(int(datetime.now().strftime("%m")))
 
+    paginacao = Paginator(tarefas, 15)
+
+    pagina_numero = request.GET.get('page')
+
+    pagina_objeto = paginacao.get_page(pagina_numero)
+
     context = {
         'titulo'   : 'Agenda menssal',
         'dia_hoje' : f'{dia_hoje[0].upper() + dia_hoje[1:]}',
-        'tarefas'  : tarefas,
+        'tarefas'  : pagina_objeto,
     }
 
     return render(
